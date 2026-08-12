fab656698912af68001f12e7a9d79284e396dab7
Shounak <ShhounakChatterjee@gmail.com>
2026-08-12 21:33:52 +0530

    Improve digit inversion logic in find_min_after_inversion
    Refactor digit inversion logic to handle leading zeros and simplify the process of choosing the smaller digit.

--- Competitive_Programming/29-07-2026/find_min_after_inversion.cpp
+++ Competitive_Programming/29-07-2026/find_min_after_inversion.cpp
@@ -1,34 +1,28 @@
 //codeforces problem 514A, Round 291 (Div. 2)
 #include <iostream>
 #include <string>
-#include <algorithm>
 using namespace std;
 
 int main() {
     string x;
-
-    cout << "Enter a 4 digit no: ";
     cin >> x;
-
-    string ans = "";
 
     for (int i = 0; i < x.size(); i++) {
         int digit = x[i] - '0';
         int inverted = 9 - digit;
 
-        if (i == 0) {
-            // First digit cannot become 0
-            if (inverted != 0 && inverted < digit)
-                digit = inverted;
-        } else {
-            // Choose the smaller value
-            digit = min(digit, inverted);
+        // First digit cannot become 0
+        if (i == 0 && inverted == 0) {
+            continue;
         }
 
-        ans += char(digit + '0');
+        // Use the smaller digit
+        if (inverted < digit) {
+            x[i] = char(inverted + '0');
+        }
     }
 
-    cout << "Answer: " << ans << endl;
+    cout << x << endl;
 
     return 0;
 }
