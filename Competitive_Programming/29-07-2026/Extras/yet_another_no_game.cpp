979ce1c252eadc93a6e86580f322ec649d46ddf7
Shounak <ShhounakChatterjee@gmail.com>
2026-08-12 21:09:03 +0530

    Add yet another no game implementation

---
+++ Competitive_Programming/29-07-2026/Extras/yet_another_no_game.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXV = 300;
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    vector<int> a(n);
+    for(auto &x : a) scanf("%d", &x);
+
+    if(n == 1){
+        puts(a[0] != 0 ? "BitLGM" : "BitAryo");
+        return 0;
+    }
+
+    if(n == 2){
+        static bool L[MAXV][MAXV];
+        static bool hasA[MAXV], hasB[MAXV], hasDiag[2*MAXV+1];
+        for(int s = 0; s <= 2*(MAXV-1); s++)
+            for(int aa = max(0, s-(MAXV-1)); aa <= min(MAXV-1, s); aa++){
+                int bb = s - aa;
+                bool isN = hasA[bb] || hasB[aa] || hasDiag[aa-bb+MAXV];
+                L[aa][bb] = !isN;
+                if(!isN){ hasA[bb]=hasB[aa]=hasDiag[aa-bb+MAXV]=true; }
+            }
+        puts(L[a[0]][a[1]] ? "BitAryo" : "BitLGM");
+        return 0;
+    }
+
+    // n == 3
+    static bool L[MAXV][MAXV][MAXV];
+    static bool hasA[MAXV][MAXV], hasB[MAXV][MAXV], hasC[MAXV][MAXV];
+    static bool hasDiag[2*MAXV+1][2*MAXV+1];
+    for(int s = 0; s <= 3*(MAXV-1); s++)
+        for(int aa = max(0, s-2*(MAXV-1)); aa <= min(MAXV-1, s); aa++){
+            int rem = s - aa;
+            for(int bb = max(0, rem-(MAXV-1)); bb <= min(MAXV-1, rem); bb++){
+                int cc = rem - bb;
+                if(cc < 0 || cc >= MAXV) continue;
+                bool isN = hasA[bb][cc] || hasB[aa][cc] || hasC[aa][bb]
+                           || hasDiag[aa-bb+MAXV][aa-cc+MAXV];
+                L[aa][bb][cc] = !isN;
+                if(!isN){
+                    hasA[bb][cc]=hasB[aa][cc]=hasC[aa][bb]=true;
+                    hasDiag[aa-bb+MAXV][aa-cc+MAXV]=true;
+                }
+            }
+        }
+    puts(L[a[0]][a[1]][a[2]] ? "BitAryo" : "BitLGM");
+    return 0;
+}
